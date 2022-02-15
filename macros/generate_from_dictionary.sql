0189fe875247789a0abcbf58eceafb5e0c1d039f
Andy Killinger <andy.killinger@gmail.com>
2022-02-14 20:28:44 -0700

    Update generate_from_dictionary.sql

--- macros/generate_from_dictionary.sql
+++ macros/generate_from_dictionary.sql
@@ -48,7 +48,7 @@
    AS
 	insert into portfolio.{@stage_table_name}
 	Select * from portfolio.vw_{@stage_table_name}
-	where cycle_date IN 
+	where as_of_date IN 
 	(
 		Select distinct as_of_date from portfolio.vw_{@stage_table_name}
 		except

ee9627549a8ddc4e4a5c55064349ef977cbaf333
Andy Killinger <andy.killinger@gmail.com>
2022-03-31 10:24:59 -0600

    Update generating_qa.sql

--- macros/generating_qa.sql
+++ macros/generating_qa.sql
@@ -13,17 +13,24 @@
    {%- endset -%}  
    
    {% set template %}
-       Select '{@table_name}' as table_name, max(as_of_date) max_date from {@schema_name}.{@table_name}
+       Select '{@table_name}' as table_name, max(as_of_date) max_date from portfolio.{@table_name}
    {% endset %}
    
-   {%- set query -%}
-      select  table_schema, table_name from internal.dictionary where lower(table_schema)='{@schema_name}'
+    {%- set query -%}
+	select  
+		DISTINCT stage_table_name
+	from internal.dictionary 
+	where 
+		database_name='{{ var('dictionary_database') }}' and version_name='{{ var('dictionary_database_version') }}' 
+		and has_column_issue=0 and has_table_issue=0
+	group by stage_table_name
+	order by stage_table_name
    {%- endset -%}
    {%- set tables = run_query(query) -%}   
    
    {% for tbl in tables %}
      {%- if not loop.first %} UNION ALL {% endif %}
-      {% do temp.append(template | string | replace('{@table_name}', tbl.TABLE_NAME)| replace('{@schema_name}', tbl.SCHEMA_NAME) ) %}
+      {% do temp.append(template | string | replace('{@table_name}', tbl.TABLE_NAME) ) %}
    {% endfor %}
 
    {% do temp.append(footer | string ) %}

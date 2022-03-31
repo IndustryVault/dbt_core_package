35b280cfe4c5eb09ec6f382c40aeb6422294e1d9
Andy Killinger <andy.killinger@gmail.com>
2022-03-31 09:47:12 -0600

    Update generating_qa.sql

--- macros/generating_qa.sql
+++ macros/generating_qa.sql
@@ -1,10 +1,33 @@
-{% macro generate_schema_name(custom_schema_name, node) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is none -%}
-        {{ default_schema }}
-    {%- elif custom_schema_name.startswith("+") -%}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
-    {%- else -%}
-        {{ custom_schema_name | trim }}
-    {%- endif -%}
-{%- endmacro %}
+
+{% macro generate_all_tables_current(schema_name) %}
+   {% set temp=[] %}
+   
+   {% set header %}
+    Select table_name, max_date from (
+   {%- endset -%}
+   {% do temp.append(header | string ) %}
+        
+   {% set footer %}
+    ) tbl
+    where max_date != CURRENT_DATE
+   {%- endset -%}  
+   
+   {% set template %}
+       Select '{@table_name}' as table_name, max(as_of_date) max_date from {@schema_name}.{@table_name}
+   {% endset %}
+   
+   {%- set query -%}
+      select  table_name from information_schema.tables where lower(table_schema)='{@schema_name}'
+   {%- endset -%}
+   {%- set tables = run_query(query) -%}   
+   
+   {% for tbl in tables %}
+     {%- if not loop.first %} UNION ALL {% endif %}
+      {% do temp.append(template | string | replace('{@table_name}', tbl.TABLE_NAME)| replace('{@schema_name}', schema_name) %}
+   {% endfor %}
+
+   {% do temp.append(footer | string ) %}
+   {% set results = temp | join ('\n') %}
+   {{ log(results, info=True) }}
+   {% do return(results) %}
+{% endmacro %}

7dc703550fee8112bfac3808fd45edd1bfe34ad3
Andy Killinger <andy.killinger@gmail.com>
2022-04-29 11:20:23 -0600

    Update vw_date_dimension.sql

--- models/internal/vw_date_dimension.sql
+++ models/internal/vw_date_dimension.sql
@@ -1,8 +1,8 @@
   {{ config(materialized='view', sort='the_date', schema='internal') }}
   
   WITH generate_dates AS (
-    SELECT DATEADD(DAY, SEQ4(), '{{ var('date_dimension_start_date') }}' ) AS the_date
-      FROM TABLE(GENERATOR(ROWCOUNT=>{{ var('date_dimension_day_count') }} ))  -- Number of days after reference date in previous line
+    SELECT DATEADD(DAY, SEQ4(), '{{ var('date_dimension_start_date', '12/31/9999') }}' ) AS the_date
+      FROM TABLE(GENERATOR(ROWCOUNT=>{{ var('date_dimension_day_count',0) }} ))  -- Number of days after reference date in previous line
   )
   SELECT the_date
         ,YEAR(the_date)::SMALLINT as year

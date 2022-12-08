6ea607faf3a9c0a4d75ad14c8adf01456c89e78b
Andy Killinger <akillinger@industryvault.com>
2022-12-08 22:30:49 +0000

    changes

--- macros/generate_raw_table_create.sql
+++ macros/generate_raw_table_create.sql
@@ -1,4 +1,4 @@
-{% macro generate_raw_table_create(database_name, version_name, table_name) %}
+{% macro generate_raw_table_create(database_name, version_name, table_name, use_source=true) %}
    {% set temp=[] %}
    {% set header %}
    {%- endset -%}
@@ -15,7 +15,8 @@ create or replace table raw.{{database_name}}.{@source_table_name}
     {%- set query -%}
   	select  
 		distinct source_table_name
-         , listagg(CONCAT('"',source_column_name,'" ',source_column_type,'\n'), ',') within group ( order by column_order) as column_list
+         , listagg(CONCAT('"',source_column_name,'" ',source_column_type,'\n'), ',') within group ( order by column_order) as source_column_list
+         , listagg(CONCAT('"',stage_column_name,'" ',stage_column_type,'\n'), ',') within group ( order by column_order) as stage_column_list
 	from {{ref('data_dictionary') }} 
 	where 
 		database_name='{{database_name}}' and version_name='{{version_name}}'  and source_table_name='{{table_name}}'
@@ -24,7 +25,11 @@ create or replace table raw.{{database_name}}.{@source_table_name}
    {%- set tables = run_query(query) -%}   
    
    {% for tbl in tables %}
-      {% do temp.append(task_template | string | replace('{@source_table_name}', tbl.SOURCE_TABLE_NAME) | replace('{@column_list_with_type}', tbl.COLUMN_LIST) ) %}
+      {% set column_list = tbl.STAGE_COLUMN_LIST %}
+      {% if use_source %}
+         {% set column_list = tbl.SOURCE_COLUMN_LIST %}
+
+      {% do temp.append(task_template | string | replace('{@source_table_name}', tbl.SOURCE_TABLE_NAME) | replace('{@column_list_with_type}', column_list) ) %}
    {% endfor %}
 
    {% set results = temp | join ('\n') %}

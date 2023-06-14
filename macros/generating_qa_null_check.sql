d77c70be16d571aec93e36a0d6e5a8e5fba9eb95
Andy Killinger <andy.killinger@gmail.com>
2023-06-14 08:40:59 -0600

    Create generating_qa_null_check.sql

---
+++ macros/generating_qa_null_check.sql
@@ -0,0 +1,36 @@
+
+{% macro check_all_tables_null(schema_name) %}
+   {% set temp=[] %}
+   
+   {% set header %}
+   {%- endset -%}
+   {% do temp.append(header | string ) %}
+        
+   {% set footer %}
+   {%- endset -%}  
+   
+   {% set template %}
+       Select '{@table_name}' as table_name, count(*) row_count, count({@column_name}) not_null_count, count(*) - count({@column_name}) null_count, max(as_of_dt) max_date, min(as_of_dt) as min_date, count from bkmrtg.{@table_name}
+   {% endset %}
+   
+    {%- set query -%}
+	select  
+		DISTINCT stage_table_name as table_name, stage_column_name as column_name
+	from internal.dictionary 
+	where 
+		database_name='{{ var('dictionary_database') }}' and version_name='{{ var('dictionary_database_version') }}' 
+	group by stage_table_name, stage_column_name
+	order by stage_table_name, stage_column_name
+   {%- endset -%}
+   {%- set tables = run_query(query) -%}   
+   
+   {% for tbl in tables %}
+     {%- if not loop.first %} UNION ALL {% endif %}
+      {% do temp.append(template | string | replace('{@table_name}', tbl.TABLE_NAME) | replace('{@column_name}', tbl.COLUMN_NAME) ) %}
+   {% endfor %}
+
+   {% do temp.append(footer | string ) %}
+   {% set results = temp | join ('\n') %}
+   {{ log(results, info=True) }}
+   {% do return(results) %}
+{% endmacro %}

759f64413949d59eb3f8d93a076ca49c97f4fd7d
Andy Killinger <andy.killinger@gmail.com>
2023-06-19 10:10:28 -0600

    Update generating_qa_null_check.sql

--- macros/generating_qa_null_check.sql
+++ macros/generating_qa_null_check.sql
@@ -10,13 +10,13 @@
    {%- endset -%}  
    
    {% set template %}
-       Select '{@table_name}' as table_name, '{@column_name}' as column_name, count(*) row_count, count({@column_name}) not_null_count
+       Select '{@table_name}' as table_name, '{@column_name}' as column_name, '{@description}' as description, count(*) row_count, count({@column_name}) not_null_count
        , count(*) - count({@column_name}) null_count, max(as_of_dt) max_date, min(as_of_dt) as min_date from bkmrtg.tb_{@table_name}
    {% endset %}
    
     {%- set query -%}
 	select  
-		DISTINCT source_table_name as table_name, source_column_name as column_name
+		DISTINCT source_table_name as table_name, source_column_name as column_name, source_column_description as description
 	from internal.data_dictionary 
 	where 
 		database_name='{{ var('dictionary_database') }}' and version_name='{{ var('dictionary_database_version') }}' 
@@ -29,7 +29,7 @@
    	{%- if not loop.first %}
 		{% do temp.append('UNION ALL' | string ) %}
 	{% endif %}
-      {% do temp.append(template | string | replace('{@table_name}', tbl.TABLE_NAME) | replace('{@column_name}', tbl.COLUMN_NAME) ) %}
+      {% do temp.append(template | string | replace('{@table_name}', tbl.TABLE_NAME) | replace('{@column_name}', tbl.COLUMN_NAME) | replace('{@description}', tbl.DESCRIPTION) ) %}
    {% endfor %}
 
    {% do temp.append(footer | string ) %}

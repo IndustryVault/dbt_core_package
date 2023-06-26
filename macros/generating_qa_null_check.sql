ad876f1c62d0c7cfbfc4e908889af625e626c63e
Andy Killinger <andy.killinger@gmail.com>
2023-06-26 14:12:19 -0600

    Update generating_qa_null_check.sql

--- macros/generating_qa_null_check.sql
+++ macros/generating_qa_null_check.sql
@@ -13,8 +13,26 @@
        Select '{@table_name}' as table_name, '{@column_name}' as column_name, '{@description}' as description, count(*) row_count, count({@column_name}) not_null_count
        , count(*) - count({@column_name}) null_count, max(as_of_dt) max_date, min(as_of_dt) as min_date from bkmrtg.tb_{@table_name}
    {% endset %}
-   
-    {%- set query -%}
+
+	{%- set query1 -%}
+	with mapping as
+(
+    select replace(upper(boddh_table_name),'TB_','') as boddh_table_name
+        ,boddh_column_name,SNOWFLAKE_TABLE_NAME, SNOWFLAKE_COLUMN_NAME, transformations
+    from public.boddh_to_snowflake b2s
+)
+        select DISTINCT 
+            SNOWFLAKE_TABLE_NAME as table_name, sNOWFLAKE_COLUMN_NAME as column_name
+         , REPLACE(source_column_description, '''','') as description
+         , transformations
+
+	from internal.data_dictionary 
+    inner join mapping on mapping.boddh_table_name=upper(source_table_name) and boddh_column_name=source_column_name
+	where 
+		 LEFT(upper(source_column_name),8) != 'PROCESS_'
+	{%- endset -%}
+	
+    {%- set query2 -%}
         select DISTINCT 
 		CASE WHEN startswith(upper(source_table_name), 'LDF_MONTHLY') THEN CONCAT(REPLACE(upper(source_table_name), '_MONTHLY',''),'_MONTHLY') 
 			ELSE source_table_name 

2d949a9806dfe952262d05379879c4a5209b8b0c
Andy Killinger <andy.killinger@gmail.com>
2024-02-09 16:05:03 -0700

    Update generate_raw_table_create.sql

--- macros/generate_raw_table_create.sql
+++ macros/generate_raw_table_create.sql
@@ -1,22 +1,22 @@
 {% macro generate_raw_table_create(database_name, version_name, table_name, use_source=true) %}
    {% set temp=[] %}
    {% set header %}
+use role load_role;
    {%- endset -%}
    {% do temp.append(header | string ) %}
 
    {% set task_template %}
-use role load_role;
 create or replace table raw.{{database_name}}.{@source_table_name} 
 (
-    {@column_list_with_type}
+{@column_list_with_type}
 );
     {% endset %}
 
     {%- set query -%}
   	select  
 		distinct source_table_name
-         , listagg(CONCAT('"',source_column_name,'" ',source_column_type,'\n'), ',') within group ( order by column_order) as source_column_list
-         , listagg(CONCAT(stage_column_name,' ',stage_column_type,'\n'), ',') within group ( order by column_order) as stage_column_list
+         , listagg(CONCAT('"',source_column_name,'" ',source_column_type), ',') within group ( order by column_order) as source_column_list
+         , listagg(CONCAT(stage_column_name,' ',stage_column_type), ',') within group ( order by column_order) as stage_column_list
 	from {{ref('data_dictionary') }} 
 	where 
 		database_name='{{database_name}}' and version_name='{{version_name}}'  and source_table_name='{{table_name}}'

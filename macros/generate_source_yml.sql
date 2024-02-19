0e1241d4a88e84c59e87b3e71949daf3a37ed597
Andy Killinger <andy.killinger@gmail.com>
2024-02-19 10:20:34 -0700

    Update generate_source_yml.sql

--- macros/generate_source_yml.sql
+++ macros/generate_source_yml.sql
@@ -50,7 +50,7 @@ sources:
 	    from internal.data_dictionary 
         where 
             database_name='{{database_name}}' and version_name='{{version_name}}' 
-	    stage_table_name like 'l%'
+	    and stage_table_name like 'l%'
             {{ apply_filter }}
 	    order by stage_table_name, column_order
     {% endset %}

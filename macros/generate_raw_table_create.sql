7c694238f633906233bac9f4d07f75071c9194b6
Andy Killinger <andy.killinger@gmail.com>
2024-02-26 09:18:46 -0700

    Update generate_raw_table_create.sql

--- macros/generate_raw_table_create.sql
+++ macros/generate_raw_table_create.sql
@@ -17,8 +17,8 @@ create or replace table raw.{{database_name}}.{@source_table_name}
     {%- set query -%}
   	select  
 		distinct source_table_name
-         , listagg(CONCAT('"',source_column_name,'" ',source_column_type), ',\n') within group ( order by column_order) as source_column_list
-         , listagg(CONCAT(stage_column_name,' ',stage_column_type), ',\n') within group ( order by column_order) as stage_column_list
+         , listagg(CONCAT('\t"',source_column_name,'" ',source_column_type), ',\n') within group ( order by column_order) as source_column_list
+         , listagg(CONCAT('\t',stage_column_name,' ',stage_column_type), ',\n') within group ( order by column_order) as stage_column_list
 	from {{ref('data_dictionary') }} 
 	where 
 		database_name='{{database_name}}' and version_name='{{version_name}}'  and source_table_name='{{table_name}}'
@@ -31,7 +31,7 @@ create or replace table raw.{{database_name}}.{@source_table_name}
       {% if use_source %}
          {% set column_list = tbl.SOURCE_COLUMN_LIST %}
       {% endif %}
-      {% do print('\t' ~ task_template | string | replace('{@source_table_name}', tbl.SOURCE_TABLE_NAME) | replace('{@column_list_with_type}', column_list) ) %}
+      {% do print(task_template | string | replace('{@source_table_name}', tbl.SOURCE_TABLE_NAME) | replace('{@column_list_with_type}', column_list) ) %}
    {% endfor %}
 	    
 {% endmacro %}

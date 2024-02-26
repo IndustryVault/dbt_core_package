f77c97607c52c2835c4b8d60750a53cde214917c
Andy Killinger <andy.killinger@gmail.com>
2024-02-26 09:25:26 -0700

    Update generate_raw_tables_load.sql

--- macros/generate_raw_tables_load.sql
+++ macros/generate_raw_tables_load.sql
@@ -1,5 +1,5 @@
 
-{% macro generate_from_dictionary_raw_tables_load (database_name='default', version_name='default', description_method='none', apply_filter='',add_lightdash=false) %}
+{% macro generate_from_dictionary_raw_tables_load (database_name='default', version_name='default') %}
 
     {% if database_name=='default' %}
         {% set database_name = var('dictionary_database', target.database) %}
@@ -11,12 +11,12 @@
 {%- set query -%}
 	
     select 
-		distinct source_table_name  
+	distinct source_table_name  
 	from {{ref('data_dictionary') }}
 	where 
 		database_name='{{ var('dictionary_database') }}' and version_name='{{ var('dictionary_database_version') }}' 
 --		and stage_column_name is not null
---        and is_public = 1
+--      and is_public = 1
     order by source_table_name
 {%- endset -%}
 

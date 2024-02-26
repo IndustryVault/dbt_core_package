93f94b8bea706dbec7deb01a5d6ffcce9123efc2
Andy Killinger <andy.killinger@gmail.com>
2024-02-26 09:02:32 -0700

    Create generate_raw_tables_create.sql

---
+++ macros/generate_raw_tables_create.sql
@@ -0,0 +1,23 @@
+
+{% macro generate_from_dictionary_raw_table_create (database_name='default', version_name='default', description_method='none', apply_filter='',add_lightdash=false) %}
+
+{%- set query -%}
+	
+    select 
+		distinct source_table_name  
+	from {{ref('data_dictionary') }}
+	where 
+		database_name='{{ var('dictionary_database') }}' and version_name='{{ var('dictionary_database_version') }}' 
+--		and stage_column_name is not null
+--        and is_public = 1
+    order by source_table_name
+{%- endset -%}
+
+{%- set rowset = run_query(query) %}    
+{% for item in rowset %} 
+    {{ iv_common.generate_raw_table_create(
+        table_name=item.SOURCE_TABLE_NAME
+    ) }}
+
+{% endfor %} 
+{%endmacro%}

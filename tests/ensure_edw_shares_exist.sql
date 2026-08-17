825764c72816079a59ec7aed7c5024c1f0f4c3a7
Andy Killinger <andy.killinger@gmail.com>
2026-08-17 10:32:41 -0600

    Update ensure_edw_shares_exist.sql

--- tests/ensure_edw_shares_exist.sql
+++ tests/ensure_edw_shares_exist.sql
@@ -1,5 +1,5 @@
 -- tests/assert_share_complete.sql
-{{ validate_share('sdw_servicervault_ut', this.database) }}
+{{ validate_share(this.database) }}
 
 -- macro raises if objects are missing; otherwise this returns 0 rows = pass
 SELECT 1 AS missing_object WHERE 1 = 0
